a178a748eb9ae99de259405c19ea4cca095441e9
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-08-20 15:00:33 +0600

    initial

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = ({}) => {
+    return (
+        <header>
+            <h1>hello</h1>
+        </header>
+    )
+}
+
+// header.defaultProps = {
+//     title: 'Task Tracker',
+// }
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Css from './app.css';
 
 ReactDOM.render(
   <React.StrictMode>
